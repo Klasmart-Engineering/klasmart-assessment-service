3e35f0bd94862f8bff75b7b744c41dea200e3c9d
cabauman <cab452005@gmail.com>
2021-06-03 15:28:27 +0900

    add basic testing setup

---
+++ tests/integration/stub.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+
+describe('tests.integration', function () {
+  it('stub', function () {
+    const result = 7
+    expect(result).equal(7)
+  })
+})
---
+++ tests/unit/stub.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+
+describe('tests.unit', function () {
+  it('stub', function () {
+    const result = 7
+    expect(result).equal(7)
+  })
+})
