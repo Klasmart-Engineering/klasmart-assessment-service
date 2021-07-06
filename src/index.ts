b41d37112ab4115152a9176350ccbf58dbd0f9f6
cabauman <cab452005@gmail.com>
2021-07-06 12:12:26 +0900

    resolve deprecation warning
    "body-parser deprecated undefined extended: provide extended option"

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ async function main() {
   app.use(compression())
   app.use(cookieParser())
   app.use(express.json({ limit: '1mb' }))
-  app.use(express.urlencoded({ limit: '1mb' }))
+  app.use(express.urlencoded({ limit: '1mb', extended: true }))
   server.applyMiddleware({
     app,
     path: routePrefix,
