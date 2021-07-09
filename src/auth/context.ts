959653baddd965d7f6bd68726066de561b3ddec3
cabauman <cab452005@gmail.com>
2021-07-09 12:15:22 +0900

    remove unused permissions code

--- src/auth/context.ts
+++ src/auth/context.ts
@@ -1,12 +1,10 @@
 import { createParamDecorator } from 'type-graphql'
-import { UserPermissions } from './permissions'
 import { IToken } from './auth'
 
 export interface Context {
   token?: IToken
   ip?: string | string[]
   userId?: string
-  permissions: UserPermissions
 }
 
 export function UserID(): ParameterDecorator {
