ac172f76565b4e934db1525894517281cf984a32
genyrosk <e.roskach@gmail.com>
2021-11-23 16:02:45 +0900

    chore: add @deprecation directives to User snake_case fields

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,15 +6,23 @@ import { getConfig, Configuration } from '../helpers/configuration'
 @ObjectType()
 export class User {
   @Field({ name: 'userId' })
-  @Field({ name: 'user_id' })
+  @Field({ name: 'user_id', deprecationReason: 'use userId instead' })
   public readonly userId!: string
 
   @Field({ name: 'givenName', nullable: true })
-  @Field({ name: 'given_name', nullable: true })
+  @Field({
+    name: 'given_name',
+    deprecationReason: 'use givenName instead',
+    nullable: true,
+  })
   public readonly givenName?: string
 
   @Field({ name: 'familyName', nullable: true })
-  @Field({ name: 'family_name', nullable: true })
+  @Field({
+    name: 'family_name',
+    deprecationReason: 'use familyName instead',
+    nullable: true,
+  })
   public readonly familyName?: string
 
   @Field({ nullable: true })
