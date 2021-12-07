64455228e2d0ce4c7c644c00e0c813dc74823b5e
cabauman <cab452005@gmail.com>
2021-12-07 13:15:20 +0900

    refactor: check cms response status and log error if unsuccessful
    Also throw error in cms APIs if authentication token is undefined

--- src/web/cms/cmsContentApi.ts
+++ src/web/cms/cmsContentApi.ts
@@ -1,6 +1,10 @@
-import fetch from 'node-fetch'
+import fetch, { Response } from 'node-fetch'
 import { Service } from 'typedi'
+import { withLogger } from 'kidsloop-nodejs-logger'
 import ContentResponse, { ContentDto } from './contentResponse'
+import { throwExpression } from '../../helpers/throwExpression'
+
+const logger = withLogger('CmsContentApi')
 
 @Service()
 export class CmsContentApi {
@@ -8,18 +12,30 @@ export class CmsContentApi {
     lessonPlanId: string,
     authenticationToken?: string,
   ): Promise<ReadonlyArray<ContentDto>> {
+    // TODO: Think about moving this check to the source (fail early).
+    if (!authenticationToken) {
+      throw new Error(
+        `[CmsContentApi.getLessonMaterials] authenticationToken is undefined.`,
+      )
+    }
     const cmsApiUrl =
-      process.env.CMS_API_URL || 'https://cms.alpha.kidsloop.net/v1/internal'
+      process.env.CMS_API_URL ?? throwExpression('CMS_API_URL is undefined')
     const contentsApiUrl = `${cmsApiUrl}/contents?plan_id=${lessonPlanId}`
 
     const fetchPromise = fetch(contentsApiUrl, {
       method: 'GET',
       headers: {
-        cookie: authenticationToken ? `access=${authenticationToken}` : '',
+        cookie: `access=${authenticationToken}`,
       },
     })
 
     const response = await fetchPromise
+    if (!response.ok) {
+      logger.error(
+        `getLessonMaterials failed. response status: ${response.status}.`,
+      )
+      return []
+    }
     const body = await response.json()
     const contentResponse = body as ContentResponse
     const contentDtos = contentResponse?.list ?? []
@@ -31,18 +47,30 @@ export class CmsContentApi {
     contentId: string,
     authenticationToken?: string,
   ): Promise<ContentDto | undefined> {
+    // TODO: Think about moving this check to the source (fail early).
+    if (!authenticationToken) {
+      throw new Error(
+        `[CmsContentApi.getLessonMaterial] authenticationToken is undefined.`,
+      )
+    }
     const cmsApiUrl =
-      process.env.CMS_API_URL || 'https://cms.alpha.kidsloop.net/v1/internal'
+      process.env.CMS_API_URL ?? throwExpression('CMS_API_URL is undefined')
     const contentsApiUrl = `${cmsApiUrl}/contents?content_ids=${contentId}`
 
     const fetchPromise = fetch(contentsApiUrl, {
       method: 'GET',
       headers: {
-        cookie: authenticationToken ? `access=${authenticationToken}` : '',
+        cookie: `access=${authenticationToken}`,
       },
     })
 
     const response = await fetchPromise
+    if (!response.ok) {
+      logger.error(
+        `getLessonMaterial failed. response status: ${response.status}.`,
+      )
+      return undefined
+    }
     const body = await response.json()
     const contentResponse = body as ContentResponse
     const contentDtos = contentResponse?.list ?? []
@@ -57,18 +85,30 @@ export class CmsContentApi {
     sourceId: string,
     authenticationToken?: string,
   ): Promise<ReadonlyArray<ContentDto>> {
+    // TODO: Think about moving this check to the source (fail early).
+    if (!authenticationToken) {
+      throw new Error(
+        `[CmsContentApi.getLessonMaterialsWithSourceId] authenticationToken is undefined.`,
+      )
+    }
     const cmsApiUrl =
-      process.env.CMS_API_URL || 'https://cms.alpha.kidsloop.net/v1/internal'
+      process.env.CMS_API_URL ?? throwExpression('CMS_API_URL is undefined')
     const contentsApiUrl = `${cmsApiUrl}/contents?source_id=${sourceId}`
 
     const fetchPromise = fetch(contentsApiUrl, {
       method: 'GET',
       headers: {
-        cookie: authenticationToken ? `access=${authenticationToken}` : '',
+        cookie: `access=${authenticationToken}`,
       },
     })
 
     const response = await fetchPromise
+    if (!response.ok) {
+      logger.error(
+        `getLessonMaterialsWithSourceId failed. response status: ${response.status}.`,
+      )
+      return []
+    }
     const body = await response.json()
     const contentResponse = body as ContentResponse
     const contentDtos = contentResponse?.list ?? []
--- src/web/cms/cmsScheduleApi.ts
+++ src/web/cms/cmsScheduleApi.ts
@@ -1,6 +1,10 @@
 import fetch from 'node-fetch'
 import { Service } from 'typedi'
+import { withLogger } from 'kidsloop-nodejs-logger'
 import ScheduleResponse, { ScheduleDto } from './scheduleResponse'
+import { throwExpression } from '../../helpers/throwExpression'
+
+const logger = withLogger('CmsScheduleApi')
 
 @Service()
 export class CmsScheduleApi {
@@ -8,18 +12,28 @@ export class CmsScheduleApi {
     scheduleId: string,
     authenticationToken?: string,
   ): Promise<ScheduleDto | undefined> {
+    // TODO: Think about moving this check to the source (fail early).
+    if (!authenticationToken) {
+      throw new Error(
+        `[CmsScheduleApi.getSchedule] authenticationToken is undefined.`,
+      )
+    }
     const cmsApiUrl =
-      process.env.CMS_API_URL || 'https://cms.alpha.kidsloop.net/v1/internal'
+      process.env.CMS_API_URL ?? throwExpression('CMS_API_URL is undefined')
     const schedulesApiUrl = `${cmsApiUrl}/schedules?schedule_ids=${scheduleId}`
 
     const fetchPromise = fetch(schedulesApiUrl, {
       method: 'GET',
       headers: {
-        cookie: authenticationToken ? `access=${authenticationToken}` : '',
+        cookie: `access=${authenticationToken}`,
       },
     })
 
     const response = await fetchPromise
+    if (!response.ok) {
+      logger.error(`getSchedule failed. response status: ${response.status}.`)
+      return undefined
+    }
     const body = await response.json()
     const scheduleResponse = body as ScheduleResponse | undefined
 
