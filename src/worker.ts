4e006da199abe2d281a9e5d2601c018ab476b97b
genyrosk <e.roskach@gmail.com>
2022-04-20 21:50:57 +0900

    refactor: move worker to root + update env vars

--- src/worker.ts
+++ src/worker.ts
@@ -3,11 +3,9 @@ import { useContainer } from 'typeorm'
 import { Container as TypeormTypediContainer } from 'typeorm-typedi-extensions'
 import { withLogger } from 'kidsloop-nodejs-logger'
 
-// import { connectToRedisCache } from '../cache/redis'
-import { connectToAssessmentDatabase } from '../db/assessments/connectToAssessmentDatabase'
-import { RedisMode, RedisStreams, connectToIoRedis } from './redisApi'
-import { STREAM_NAME, GROUP_NAME } from './index'
-import { simpleConsumerGroupWorker } from './simpleConsumerGroupWorker'
+import { connectToAssessmentDatabase } from './db/assessments/connectToAssessmentDatabase'
+import { RedisMode, RedisStreams, connectToIoRedis } from './streams/redisApi'
+import { simpleConsumerGroupWorker } from './streams/simpleConsumerGroupWorker'
 
 useContainer(TypeormTypediContainer)
 
@@ -19,14 +17,15 @@ const main = async () => {
   const redisMode = (process.env.REDIS_MODE || 'NODE').toUpperCase()
   const redisPort = Number(process.env.REDIS_PORT) || 6379
   const redisHost = process.env.REDIS_HOST
-  const streamName = process.env.REDIS_STREAM || 'xapi:events'
-  const errorStreamName = process.env.REDIS_ERROR_STREAM || 'xapi:events:error'
+  const stream = process.env.REDIS_STREAM || 'xapi:events'
+  const errorStream = process.env.REDIS_ERROR_STREAM || 'xapi:events:error'
 
   const redisConfiguredCorrectly =
     redisHost &&
     redisPort &&
     ['NODE', 'CLUSTER'].includes(redisMode) &&
-    streamName
+    stream &&
+    errorStream
 
   if (!redisConfiguredCorrectly) {
     throw new Error(
@@ -40,9 +39,8 @@ const main = async () => {
     redisPort,
   )
   const xClient = new RedisStreams(redisClient)
-  const stream = STREAM_NAME
-  const group = GROUP_NAME
-  const consumer = process.env.CONSUMER_NAME || 'assessment-worker'
+  const group = process.env.REDIS_CONSUMER_GROUP || 'assessment-worker'
+  const consumer = process.env.REDIS_CONSUMER || 'worker-0'
 
   const assessmentDatabaseUrl = process.env.ASSESSMENT_DATABASE_URL
   if (!assessmentDatabaseUrl) {
@@ -61,18 +59,16 @@ const main = async () => {
 
   // infinite process
   logger.info('🌭 Assessment Worker ready to consume xapi events')
-  simpleConsumerGroupWorker(
-    xClient,
-    streamName,
-    errorStreamName,
-    group,
-    consumer,
-    {
-      minEvents: 0,
-      maxDelays: 0,
-      retryWhenFailed: true,
-    },
-  )
+  logger.info(`Stream: ${stream}`)
+  logger.info(`Error stream: ${errorStream}`)
+  logger.info(`Consumer Group: ${group}`)
+  logger.info(`Consumer: ${consumer}`)
+  logger.info(`Retry when failed: ${true}`)
+  simpleConsumerGroupWorker(xClient, stream, errorStream, group, consumer, {
+    minEvents: 0,
+    maxDelays: 1,
+    retryWhenFailed: true,
+  })
 }
 
 main()
